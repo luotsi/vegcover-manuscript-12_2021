561dd8b227ad5ffdee27bc6003d94272be409ea5
Markku Luotamo <markku.luotamo@reaktor.com>
2021-12-25 16:46:46 +0200

    Tweak log messages

--- run_pipeline_docker.sh
+++ run_pipeline_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Provide config file placed in repo root dir as first arg $1
-# Default is the SAR experiment. See and try msi_sar_fusion_6500.yml for a more advanced example. Write your own or modify parameters for a different pipeline config.
+# Default is a MSI-SAR data fusion experiment using most features of the pipeline. See and try sar_10_to_300.yml for a more bare-bones example. Write your own or modify parameters for a different pipeline config.
 
-PIPELINE_CONFIG=${1:-sar_10_to_300.yml} 
+PIPELINE_CONFIG=${1:-msi_sar_fusion_6500.yml} 
 echo Running pipeline for config $PIPELINE_CONFIG ...   
-docker run -v $(pwd)/out:/out -it luotsi/vegcover python ./pipeline.py -t sar_10_to_300.yml; chown -R $(id -u) /out
+docker run -e PROJ_LIB=/usr/local/include/proj -v $(pwd)/out:/out -it luotsi/vegcover python ./pipeline.py -t $PIPELINE_CONFIG
