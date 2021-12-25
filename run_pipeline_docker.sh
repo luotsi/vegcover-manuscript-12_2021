a72771ecced9a93b770e20a6d06d4dc24c5fc9a8
Markku Luotamo <markku.luotamo@orbitgo.com>
2021-12-25 14:11:08 +0200

    Add bash script for starting docker

--- run_pipeline_docker.sh
+++ run_pipeline_docker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Provide config file placed in repo root dir as first arg $1
-PIPELINE_CONFIG=$1
-if "$PIPELINE_CONFIG" == "":
-   PIPELINE_CONFIG=sar_10_to_300.yml # default 
-fi
-   
-docker run -v $(pwd)/out:/out -it vegcover bash ./pipeline.py -t sar_10_to_300.yml; chmod o+rw /out
+# Default is the SAR experiment. See and try msi_sar_fusion_6500.yml for a more advanced example. Write your own or modify parameters for a different pipeline config.
+
+PIPELINE_CONFIG=${1:-sar_10_to_300.yml} 
+echo Running pipeline for config $PIPELINE_CONFIG ...   
+docker run -v $(pwd)/out:/out -it luotsi/vegcover python ./pipeline.py -t sar_10_to_300.yml; chown -R $(id -u) /out
